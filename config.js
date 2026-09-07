bd475cd62615cf901c0a0afbf12a161e3a739efc
Mathias De Vos <mathias.devos1@gmail.com>
2026-09-07 17:19:17 +0200

    Fix URL formatting in TIKKLOK_CONFIG

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
    geen toegang tot je uren. Die zijn beveiligd met Row Level Security,
    zodat enkel jouw eigen aangemelde account je eigen rijen kan lezen. */
 window.TIKKLOK_CONFIG = {
-  url:     "https://ckwvcylqvwegdrkwlujg.supabase.co/rest/v1/",   // bv. "https://abcdefghijkl.supabase.co"
+  url:     "https://ckwvcylqvwegdrkwlujg.supabase.co",   // bv. "https://abcdefghijkl.supabase.co"
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNrd3ZjeWxxdndlZ2Rya3dsdWpnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg3ODkzMjYsImV4cCI6MjEwNDM2NTMyNn0.jV80yscpIkQMJ1krNf4zyh6wZpcs8Mv0c_cQzTb9wSU"    // bv. "eyJhbGciOiJIUzI1NiIs..."
 };
